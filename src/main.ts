6f2ea3adb2a06bf4a444d30bc044b8e40e247a8b
Wilk Franca <wilk.caetano@gmail.com>
2021-06-24 23:37:20 -0300

    add process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidateInputPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
